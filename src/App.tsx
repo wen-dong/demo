3db5d23e1d514ee72890c227a0795b0af2522a43
余文东 <zn_yuwd@corp.21cn.com>
2021-04-04 18:20:13 +0800

    change

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"><img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header"><img
+        src={logo} className="App-logo" alt="logo" />
         <p>
           Edit3 <code>src/App.tsx</code> and save to reload.
         </p>
