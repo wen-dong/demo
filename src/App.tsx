6dd336b38ed0e2292b65253360a5f7e0afd6baf3
余文东 <zn_yuwd@corp.21cn.com>
2021-04-04 18:03:07 +0800

    feat: all

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header"><img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit2 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
